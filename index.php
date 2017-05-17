522b73d3f116b675ccaa0932d5aa2ac04dc2b866
Valerie Jardin <valerie.jardin60@gmail.com>
2017-05-17 09:18:40 +0200

    Partie 8 du php avec changement sur la navbar

--- index.php
+++ index.php
@@ -3,8 +3,8 @@
         <ul class="nav navbar-nav">
             <li><a href="../exercice1/index.php">Exercice 1</a></li>
             <li><a href="../exercice2/index.php">Exercice 2</a></li>
+            <li><a href="../exercice2/indexLink.php">Exercice 2 page 2</a></li>
             <li><a href="../exercice3/index.php">Exercice 3</a></li>
-            <li><a href="../exercice3Bonus/index.php">Exercice 3 Bonus</a></li>
             <li><a href="../exercice4/index.php">Exercice 4</a></li>
             <li><a href="../exercice5/index.php">Exercice 5</a></li>
         </ul>
